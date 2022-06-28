b92eece722a662e5a8f9f1daf228f9adfcb70406
Dagg <daggintosh@outlook.com>
2022-06-28 10:33:45 -0700

    Design test

--- lcj3/ContentView.swift
+++ lcj3/ContentView.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        NavigationView {
+            List {
+                NavigationLink(destination: ContentView()) {
+                    VStack() {
+                        Image(systemName: "ellipsis").resizable().aspectRatio(16/9, contentMode: .fit)
+                        HStack() {
+                            VStack(alignment: .leading) {
+                                Text("Hello, world this is a long title, a very long title").font(.callout)
+                                Text("Long Name of a Creator on the Platform").font(.caption)
+                            }
+                            Spacer()
+                            Text("100B\n views").multilineTextAlignment(.center).font(.subheadline)
+                        }
+                        
+                    }
+                }
+                NavigationLink(destination: ContentView()) {
+                    VStack() {
+                        Image(systemName: "ellipsis").resizable().aspectRatio(16/9, contentMode: .fit)
+                        HStack() {
+                            VStack(alignment: .leading) {
+                                Text("Sensible Title Numer 4029").font(.callout)
+                                Text("Fontoso Creator 39").font(.caption)
+                            }
+                            Spacer()
+                            Text("493K\n views").multilineTextAlignment(.center).font(.subheadline)
+                        }
+                        
+                    }
+                }
+            }.navigationTitle("Fontoso").navigationBarTitleDisplayMode(.inline)
+            
+        }.navigationViewStyle(DoubleColumnNavigationViewStyle())
     }
 }
 
