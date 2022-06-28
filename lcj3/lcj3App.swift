cae6160599caaae9a459fd8a2f024e91aeb15aed
Dagg <daggintosh@outlook.com>
2022-06-28 10:10:55 -0700

    Initial Commit

---
+++ lcj3/lcj3App.swift
@@ -0,0 +1,17 @@
+//
+//  lcj3App.swift
+//  lcj3
+//
+//  Created by Dagg on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct lcj3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
